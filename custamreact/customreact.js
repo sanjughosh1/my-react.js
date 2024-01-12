6fe2cc7f52a877901ecfdf16c7b0cd618dec8e1b
sanju <sanjughosh395@gmail.com>
2024-01-12 17:02:06 +0530

    converson of datatipe

--- custamreact/customreact.js
+++ custamreact/customreact.js
@@ -1,18 +1,73 @@
-function customeRender(ReractElement,maincontainor){
-    const domElement = document.createElement(ReractElement.type)
-    domElement.innerHTML = ReractElement.children
-    domElement.setAttribute('href',ReractElement.props.href)
-    domElement.setAttribute('terget', ReractElement.props.target)
-    containor.appendChild(domElement)
-}
+// function customeRender(ReractElement,maincontainor){
+//     const domElement = document.createElement(ReractElement.type)
+//     domElement.innerHTML = ReractElement.children
+//     domElement.setAttribute('href',ReractElement.props.href)
+//     domElement.setAttribute('terget', ReractElement.props.target)
+//     containor.appendChild(domElement)
+// }
 
-const ReractElement = {
-    type: 'a',
-    props:{
-        href:'https://google.com',
-        target :'_blank'
-    },
-    children:'clickme to visit google'
-}
+// const ReractElement = {
+//     type: 'a',
+//     props:{
+//         href:'https://google.com',
+//         target :'_blank'
+//     },
+//     children:'clickme to visit google'
+// }
 
-const maincontainor= document.querySelector('#root')
+// const maincontainor= document.querySelector('#root')
+
+
+// data tipe
+
+// let name = "sanju"
+// let age = 22
+// let isLogin = true
+
+//number => 2 to power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null => standalone value
+//undefine => valunot given
+//sumbol => unique
+
+// console.log(typeof null) //output object
+// console.log(typeof undefined) //output undefine
+
+
+//++++++++++++++++++++++++++++++ sec ++++++++++++++++++++++++++++++
+
+
+//let score = "450abc"
+// let score = null
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true/false => 1 or 0
+
+
+
+let isLogin = " sanju"
+
+let booleanIsLoggedIn = Boolean(isLogin)
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => folse
+// "" =>folse ; "sanju" => true
+
+
+let soumNumber = 333
+
+let stringNumber = String(soumNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
