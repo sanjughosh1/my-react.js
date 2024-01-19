da5dc27e30166ab8aaf805a69ee270a6dbf5a8ea
sanju <sanjughosh395@gmail.com>
2024-01-19 19:57:10 +0530

    about dates

---
+++ custamreact/Dates.js
@@ -0,0 +1,19 @@
+// let myDate = new Date()
+// console.log(myDate.toString()) //Fri Jan 19 2024 19:30:19 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString())//Fri Jan 19 2024
+// console.log(myDate.toISOString())//2024-01-19T14:00:19.371Z
+// console.log(myDate.toJSON())//2024-01-19T14:00:19.371Z
+// console.log(myDate.toLocaleDateString())// 19/1/2024
+// console.log(myDate.toLocaleString())// 19/1/2024, 7:30:19 pm
+// console.log(myDate.toLocaleTimeString())// 7:30:19 pm
+// console.log(typeof myDate) //object
+
+
+// let creatMyDate = new Date(2024, 0, 13)
+// let creatMyDate = new Date(2024, 0, 13, 2, 15, 55)
+let creatMyDate = new Date("01-14-2024")
+
+//console.log(creatMyDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+console.log()
